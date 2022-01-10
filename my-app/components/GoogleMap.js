bb78e74e01109f7b99e261191f8fe6638763f8fd
ilyasgedikoglu <ilyas@turna.tech>
2022-01-10 21:59:41 +0300

    initial commit

--- my-app/components/GoogleMap.js
+++ my-app/components/GoogleMap.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import GoogleMapReact from "google-map-react";
+import Axios from "axios";
+
 import pin from "../pin.png";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -78,30 +80,107 @@ function SimpleMap(props) {
     const [zoom, setZoom] = useState(16)
     const [lat, setLat] = useState(center.lat)
     const [lng, setLng] = useState(center.lng)
+    const [address, setAddress] = useState("");
+    const [mapInstance, setMapInstance] = useState(null);
+    const [mapApi, setMapApi] = useState(null);
 
+    const createMapOptions = (maps) => {
+        return {
+            panControl: false,
+            mapTypeControl: false,
+            scrollwheel: true,
+            styles: [{ stylers: [{ 'saturation': -100 }, { 'gamma': 0.8 }, { 'lightness': 4 }, { 'visibility': 'on' }] }]
+        }
+    }
+
+    const apiHasLoaded = (map, maps) => {
+        setMapApi(maps);
+        setMapInstance(map);
+        console.log(map);
+        console.log(maps);
+
+        //setMapApi(maps);
+        //setMapApiLoaded(true);
+        //setMapInstance(map)
+    };
+
+    //haritada herhangi bir noktaya basınca lat ve lng değerleri alınıyor
     function click(e) {
         setLng(e.lng);
         setLat(e.lat)
     }
 
+    function clearSearchBox() {
+        setAddress('');
+    }
+
+    function searchAddress() {
+        const config = {
+            method: 'get',
+            url: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + address +"&key=AIzaSyDUbXD4z3w1UoiCOxaAj0RFt3pT24k88O4",
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        };
+
+        Axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
-        <div style={{ height: "100vh", width: "100%" }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{key: ""}}
-                defaultCenter={center}
-                defaultZoom={zoom}
-                draggable={true}
-                onClick={(e) => click(e)}
-                yesIWantToUseGoogleMapApiInternals
-            >
-                <Marker
-                    key={1}
-                    lat={lat}
-                    lng={lng}
-                    place={""}
-                    show={false}
-                />
-            </GoogleMapReact>
+        <div>
+            {
+                props.search &&
+                    <>
+                        <input
+                            type="text"
+                            onFocus={() => clearSearchBox}
+                            onChange={e => setAddress(e.target.value.trim())}
+                            placeholder="Enter a address"
+                        />
+                        <button onClick={() => searchAddress()}>Ara</button>
+                    </>
+            }
+            <div style={{ height: "100vh", width: "100%" }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: ""}}
+                    defaultCenter={center}
+                    defaultZoom={zoom}
+                    draggable={true}
+                    onClick={(e) => click(e)}
+                    yesIWantToUseGoogleMapApiInternals
+                    options={createMapOptions}
+                    onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)}
+                >
+                    {
+                        props.locations && props.locations.length > 0 ?
+                            props.locations.map((item, i) =>
+                                item.address.length > 0 &&
+                                item.address.map((address, z) =>
+                                    <Marker
+                                        key={address.id}
+                                        lat={address.lat}
+                                        lng={address.lng}
+                                        place={item}
+                                        show={false}
+                                    />
+                                )
+                            ) :
+                            <Marker
+                                key={1}
+                                lat={lat}
+                                lng={lng}
+                                place={""}
+                                show={false}
+                            />
+                    }
+                </GoogleMapReact>
+            </div>
         </div>
     );
 }
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -3,9 +3,11 @@ import SimpleMap from "../components/GoogleMap";
 const locations = require("../locations.json");
 
 function Home () {
+    //locations gönderirsek addresleri listeliyor göndermezsek harita üzerinden işaretleyip lat ve lng değerleri alınıyor.
+    //arama için search=true parametresini gönder. Arama yoksa false gönder
     return (
         <div className="App">
-            <SimpleMap locations={locations} />
+            <SimpleMap search={true} locations={locations} />
         </div>
     );
 }
